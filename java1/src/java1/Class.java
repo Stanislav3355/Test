fe771cd543c0e755b9796f17eabe0d961db6d99b
Stas <stas.heavy.metal@gmail.com>
2016-04-12 14:52:49 +0300

    initial

---
+++ java1/src/java1/Class.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=5 , j=10;
+String str = "My first string"
+	}
+	System.out.println(str);
+
+}
